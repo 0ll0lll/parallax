57fb8f5a0178cc95a940df59483a0796bbad3988
Dmitrii Koshelev <delta0ll0lll@gmail.com>
2022-11-25 02:46:51 +0000

    Added drag functionality to mobile

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useMemo } from 'react';
 import { useGesture } from 'react-use-gesture';
+import { isMobile } from 'react-device-detect';
 import '../styles/map.css';
 import Tile from './Tile';
 
@@ -32,33 +33,64 @@ const map = () => {
     'y',
     'z'
   ];
-  const shuffledLetters = useMemo(() => letters.sort(() => Math.random() - 0.5), []);
+  const mapContainerRef = useRef();
   const mapRef = useRef();
+  const shuffledLetters = useMemo(() => letters.sort(() => Math.random() - 0.5), []);
   const [crop, setCrop] = useState({ x: -window.innerWidth / 2, y: -window.innerHeight / 2 });
+  const [color, setColor] = useState();
 
   useGesture(
     {
       onMove: ({ xy }) => {
+        if (isMobile) return;
         setCrop({ x: -xy[0], y: -xy[1] });
+      },
+      onDrag: ({ movement: [dx, dy] }) => {
+        if (!isMobile) return;
+        setColor('');
+        setCrop((prev) => ({ ...prev, x: dx, y: dy }));
+      },
+      onDragEnd: () => {
+        const newCrop = crop;
+        const mapBounds = mapRef.current.getBoundingClientRect();
+        const mapContainerBounds = mapContainerRef.current.getBoundingClientRect();
+
+        if (mapBounds.left > mapContainerBounds.left) {
+          newCrop.x = 0;
+        } else if (mapBounds.right < mapContainerBounds.right) {
+          newCrop.x = -(mapBounds.width - mapContainerBounds.width);
+        }
+
+        if (mapBounds.top > mapContainerBounds.top) {
+          newCrop.y = -40;
+        } else if (mapBounds.bottom < mapContainerBounds.bottom) {
+          newCrop.y = -(mapBounds.height - mapContainerBounds.height);
+        }
+        setColor();
+        setCrop(newCrop);
       }
     },
     {
+      drag: {
+        initial: () => [crop.x, crop.y]
+      },
       domTarget: mapRef
     }
   );
 
   return (
-    <div className="relative z-10 h-full overflow-hidden">
+    <div ref={mapContainerRef} className="relative z-10 h-full overflow-hidden">
       <div
         ref={mapRef}
         id="map"
         style={{
-          transform: `translate(calc(${crop.x}px), calc(${crop.y}px))`
+          transform: `translate(${crop.x}px, ${crop.y}px)`,
+          backgroundColor: color
         }}
         className="relative top-10"
       >
         {shuffledLetters.map((letter, index) => (
-          <Tile letter={letter} index={index} />
+          <Tile key={letter} letter={letter} index={index} />
         ))}
 
         <p className="absolute top-0 left-0 font-bold bg-red-500"> TOP LEFT</p>
