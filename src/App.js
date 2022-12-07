2eea279cce77a0209b3bab9aebb25a2faf4c9152
Dmitrii Koshelev <delta0ll0lll@gmail.com>
2022-12-07 18:13:32 +0000

    Added intro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { GameWrapper } from './contexts/GameContext';
-import Game from './components/Game';
+import Intro from './components/Intro';
 import Map from './components/Map';
 import Header from './components/Header';
 import Santa from './components/Santa';
 
 function App() {
   const [isGameOn, setIsGameOn] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <GameWrapper>
       <div>
+        {showIntro && <Intro />}
         <Header isGameOn={isGameOn} setIsGameOn={setIsGameOn} />
         <main style={{ height: 'calc(100vh - 3rem)' }} className="bg-beige">
           <Map isGameOn={isGameOn} />
